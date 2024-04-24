e12c44e8085bcf4535f04c5512ab6e71f7f12211
dream023se <dubeysanchika82@gmail.com>
2024-04-24 14:15:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar';
+
+
+function App() {
+  return (
+  <>
+   <Navbar title="Sanchika" aboutText="About you"/>
+    </>
+  );
+}
+
+export default App;
